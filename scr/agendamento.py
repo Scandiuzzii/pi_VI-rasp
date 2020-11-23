1db2bd530b139a3133a70304d1b8122bd4ecc8c9
fexd12 <felipe.demelo@hotmail.com>
2020-11-23 20:38:45 -0300

    ultimas alterações

--- scr/agendamento.py
+++ scr/agendamento.py
@@ -8,15 +8,15 @@ from pi.leitura_tag import ler
 def reles(acesso):
     try:
         if acesso == 1: #liga tudo
-            print('acesso 1')
+            #print('acesso 1')
             GPIO.output(pino_rele_1,0)
             GPIO.output(pino_rele_2,0)
         elif acesso == 0: # liga lampada
-            print('acesso 0')
+            #print('acesso 0')
             GPIO.output(pino_rele_1,0)
             GPIO.output(pino_rele_2,1)
         elif acesso == 3: #desliga tudo
-            print('acesso 3')
+            #print('acesso 3')
             GPIO.output(pino_rele_1,1)
             GPIO.output(pino_rele_2,1)
     except Exception as e:
@@ -28,9 +28,9 @@ def time_atual():
 
     data_atual = str(data.year) + '-' + str(data.month) + '-' + str(data.day)
 
-    hora_atual = str(data.hour) + ':' + str(data.minute)
+    hora_atual = datetime.time(hour=data.hour,minute=data.minute,second=0)
 
-    return data_atual,hora_atual
+    return hora_atual
 
 def enviar_feedback(agendamento):
     try:
@@ -61,34 +61,47 @@ print('iniciando leitura agendamento')
 while True:
     try:
         tag,_ = ler()
-
+        
         response = requests.get(conf.HOST + '/tag/agendamento/' + str(tag))  # pegar agendamento
-        agendamento = response.json()
+        content = response.json()
+        print(tag)
+        agendamento = content['items'][0]
         print(agendamento)
+        
+        hora_atual = time_atual()
+        
+        
+        if agendamento['acesso'] == 1:
+            print('acesso_1')
+            reles(1) #
+            horario_final = datetime.datetime.strptime(agendamento['horario_final'],'%H:%M:%S')
+            
+            while True:
+                if horario_final.time() == hora_atual:
+                    break
 
-        data_atual,hora_atual  = time_atual()
-
-        if agendamento['acesso'] and agendamento['data'] == data_atual and agendamento['hora'] == hora_atual:
-            print('acesso 1')
-            while agendamento['hora_final'] == hora_atual:
-                reles(1)
-                time.sleep(60)
-                _,hora_atual = time_atual()
+                #time.sleep(60)
+                hora_atual = time_atual()
+            print('desligado')
             reles(3)
             enviar_feedback(agendamento['id_agendamento'])
 
-        elif not agendamento['acesso'] and agendamento['data'] == data_atual and agendamento['hora'] == hora_atual:
-            print('acesso 0')
-            while agendamento['hora_final'] == hora_atual:
-                reles(0)
-                time.sleep(60)
-                _,hora_atual = time_atual()
-            reles(3)
-            enviar_feedback(agendamento['id_agendamento'])
+        elif agendamento['acesso'] == 2:
+            reles(0)
+            tag_segunda,_ = ler()
+            if tag == tag_segunda:
+                reles(3)
+
+        elif agendamento['acesso'] == 3:
+            reles(1)
+            tag_segunda,_ = ler()
+            if tag == tag_segunda:
+                reles(3)
 
         else:
             raise 'erro ao liberar acesso'
 
     except Exception as e:
-        GPIO.cleanup()
         print(e)
+        GPIO.cleanup()
+        
